7b04b2315c567dd77fb95d7357aaac9c45863c44
Ian Kisali <iankisali@gmail.com>
2023-06-13 20:38:15 +0300

    fewest number of coins needed to meet a given amount total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values,
+    determining the fewest number of coins needed to meet a given amount total"""
+    if total <= 0:
+        return 0
+    pile = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for x in range(coin, total + 1):
+            pile[x] = min(pile[x], pile[x - coin] + 1)
+    return pile[-1] if pile[-1] != float("inf") else -1
